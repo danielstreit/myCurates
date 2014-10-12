199151672194b138f9efc6c55378af3586cce3c6
Daniel Streit <Daniel.Streit@gmail.com>
2014-10-11 17:48:55 -0700

    [feat] Updates collection model

--- server/api/collection/collection.model.js
+++ server/api/collection/collection.model.js
@@ -4,9 +4,42 @@ var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 var CollectionSchema = new Schema({
-  name: String,
-  info: String,
-  active: Boolean
+  title: {
+    type: String,
+    required: true
+  },
+  url: {
+    type: String,
+    required: true
+  },
+  user: {
+    type: {
+      name: String,
+      _id: String
+    },
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  links: [{
+    url: String,
+    title: String,
+    description: String
+  }],
+  userStars: {
+    type: [],
+    default: []
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model('Collection', CollectionSchema);
