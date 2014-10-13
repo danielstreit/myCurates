354560ea0660d378683bbe9822d6fa4be0837258
Daniel Streit <Daniel.Streit@gmail.com>
2014-10-12 22:34:38 -0700

    [refactor] production variables

--- dist/server/config/environment/production.js
+++ dist/server/config/environment/production.js
@@ -11,7 +11,7 @@ module.exports = {
 
   // MongoDB connection options
   mongo: {
-    uri:    process.env.MONGOHQ_URI ||
+    uri:    process.env.MONGOLAB_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/curates'
