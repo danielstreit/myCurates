b7372e631a201d18bfd69a01e61922f69208b796
Daniel Streit <Daniel.Streit@gmail.com>
2014-10-12 21:29:47 -0700

    [feat] set production variables

--- dist/server/config/environment/production.js
+++ dist/server/config/environment/production.js
@@ -4,18 +4,14 @@
 // =================================
 module.exports = {
   // Server IP
-  ip:       process.env.OPENSHIFT_NODEJS_IP ||
-            process.env.IP ||
-            undefined,
+  ip: process.env.IP || undefined,
 
   // Server port
-  port:     process.env.OPENSHIFT_NODEJS_PORT ||
-            process.env.PORT ||
-            8080,
+  port: process.env.PORT || 8080,
 
   // MongoDB connection options
   mongo: {
-    uri:    process.env.MONGOLAB_URI ||
+    uri:    process.env.MONGO_URI ||
             process.env.MONGOHQ_URL ||
             process.env.OPENSHIFT_MONGODB_DB_URL+process.env.OPENSHIFT_APP_NAME ||
             'mongodb://localhost/curates'
